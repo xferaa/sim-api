f0b787375a4d1ecffaf556f12c9e11a6be465706
Ismael Carreno <icarreno@lstelcom.com>
2017-07-27 18:05:05 +0200

    Entity Framework Sql Server config

--- src/SimApi.Data.EFCore/SqlServerUnitOfWork.cs
+++ src/SimApi.Data.EFCore/SqlServerUnitOfWork.cs
@@ -1,19 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace SimApi.Data.EFCore
 {
     public class SqlServerUnitOfWork : BaseUnitOfWork
     {
+        public SqlServerUnitOfWork() : this(CreateContext(string.Empty))
+        {
+        }
+
+        public SqlServerUnitOfWork(string nameOrConnectionString) : this(CreateContext(nameOrConnectionString))
+        {
+        }
+
         public SqlServerUnitOfWork(SimApiContext dbContext) : base(dbContext)
         {
         }
 
-        private static SimApiContext CreateContext()
+        private static SimApiContext CreateContext(string nameOrConnectionString)
         {
-            return new SimApiContext()
+            var builder = new DbContextOptionsBuilder<SimApiContext>();
+            builder.UseSqlServer(nameOrConnectionString);
+            return new SimApiContext(builder.Options);
         }
     }
 }
